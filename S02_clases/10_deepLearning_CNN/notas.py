7157e4f4e82b764517d6dc63937a46a58140b3cf
Fernando-JAL <alcantaralopezfernando@gmail.com>
2025-05-20 16:24:05 -0600

    new changes

--- S02_clases/10_deepLearning_CNN/notas.py
+++
@@ -1,8 +0,0 @@
-# Importing the Keras libraries and packages
-from keras.models import Sequential
-from keras.layers import Conv2D
-from keras.layers import MaxPooling2D
-from keras.layers import Flatten
-from keras.layers import Dense
-
-print('Hola')
